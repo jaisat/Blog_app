a4fefed605b51a06512e083c20cadf2ebac84864
jaisat <jaiswalsatyam211@gmail.com>
2021-05-09 23:11:57 +0530

    Routes and style added

---
+++ app.js
@@ -0,0 +1,66 @@
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
+// APP CONFIG
+mongoose.connect("mongodb://localhost/blog_app",{useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+// MONGOOSE CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date,default: Date.now}  
+});
+
+var Blog = mongoose.model("Blog",blogSchema);
+
+// RESTFUL ROUTES
+
+app.get("/blogs",function(req,res){
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index",{blogs:blogs});
+        }
+    });
+});
+
+app.get("/blogs/new",function(req,res){
+    res.render("new");
+});
+
+app.post("/blogs",function(req,res){
+    Blog.create(req.body.blog,function(err,newBlog){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOE ROUTE
+
+app.get("/blogs/:id",function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("show",{blog:foundBlog});
+        }
+    });
+});
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("SERVER HAS STARTED!!");
+});
